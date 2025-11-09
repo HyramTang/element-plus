05c486fd817eb28338d47b4da7230973347e4fe6
Hyram Tang <hyram.tang@gmail.com>
2025-11-09 15:06:12 +0800

    feat(components): [table] 设置 border 为 true

--- packages/components/table/src/table/defaults.ts
+++ packages/components/table/src/table/defaults.ts
@@ -236,7 +236,10 @@ export default {
   /**
    * @description whether Table has vertical border
    */
-  border: Boolean,
+  border: {
+    type: Boolean,
+    default: true,
+  },
   /**
    * @description key of row data, used for optimizing rendering. Required if `reserve-selection` is on or display tree data. When its type is String, multi-level access is supported, e.g. `user.info.id`, but `user.info[0].id` is not supported, in which case `Function` should be used
    */
